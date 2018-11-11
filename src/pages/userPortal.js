419f67a6e8887180a9c4a91c07acbb2b86ad9acd
kev-th <kevinkthaw@gmail.com>
2018-11-11 15:54:21 -0500

    merge header,sidebar and footer

--- src/pages/userPortal.js
+++ src/pages/userPortal.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Footer from '../components/Footer';
-import SideBar from '../components/SideBar'
+import SideBar from '../components/SideBar';
+import Header from '../components/header';
 
 class UserPortal extends Component{
 
     render(){
         return(
             <div className="User-Portal">
+                <Header />
                 <SideBar/>
                 <Footer/>
             </div>
