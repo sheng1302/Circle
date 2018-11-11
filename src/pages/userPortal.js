eca1da1d47fffb32182939437ce98e9f12276eaf
kev-th <kevinkthaw@gmail.com>
2018-11-11 16:05:42 -0500

    Resolve conflicts

--- src/pages/userPortal.js
+++ src/pages/userPortal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Footer from '../components/Footer';
 import SideBar from '../components/SideBar'
-import Header from '../components/header';
+import Header from '../components/Header';
 
 class UserPortal extends Component{
 
