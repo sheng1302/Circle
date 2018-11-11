3bd519d2c33e40d740dac3eb08a6d96470170a65
Sheng Liu <sheng1302@gmail.com>
2018-11-11 12:14:01 -0500

    footer component added

---
+++ src/components/footer.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import logo from '../sources/circle_logo.png';
+import '../styles/footer.css'; // using footer style
+
+class Footer extends Component{
+
+
+    render() {
+        return(
+            <div className="company-background-container">
+
+                <div className="container-section-one">
+                    <img className="circle-logo" src={logo} alt="Circle Logo"/>
+                </div>
+
+                <div className="container-section-two">
+                    <p className="section-two-copyright">
+                        Copyright © 2018 Circle. All Right Reserved.
+                    </p>
+
+                </div>
+
+            </div>
+        )
+    }
+
+}
+
+export default Footer;
---
+++ src/pages/userPortal.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Footer from '../components/footer';
+
+class UserPortal extends Component{
+
+    render(){
+        return(
+            <div className="User-Portal">
+
+                <Footer/>
+            </div>
+        )
+    }
+}
+
+export default UserPortal;
