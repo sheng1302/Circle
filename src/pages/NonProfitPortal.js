94d241b8c2bbd8c3157def03bbe9afb102e1257f
kev-th <kevinkthaw@gmail.com>
2018-11-26 17:31:29 -0500

    populate item from db

--- src/pages/NonProfitPortal.js
+++ src/pages/NonProfitPortal.js
@@ -5,8 +5,26 @@ import ItemBox from '../components/ItemBox';
 
 
 class NonProfitPortal extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            items : [],
+        }
+    }
+
+    componentDidMount(){
+        fetch('/items')
+        .then((resp)=>{
+            return resp.json();
+        }).then((result)=>{
+            this.setState({
+                items: result,
+            })
+        })
+    }
 
     render(){
+        
         return(
             <div className="NonProfit-Portal">
                 <div className="body-container">
@@ -15,36 +33,18 @@ class NonProfitPortal extends Component{
                         <SideBar/>
                     </div>
 
-                    <div className="col-md-9">
-                        <div className="row">
-                            <div className="col-md-3">
-                                <ItemBox message="this is an item box" />
-                            </div>
-                            <div className="col-md-3">
-                                <ItemBox message="this is an item box" />
-                            </div>
-                            <div className="col-md-3">
-                                <ItemBox message="this is an item box" />
-                            </div>
-                        </div>
+                    <div>
+                        {this.state.items.map((item) => {
+                            return <ItemBox title={item.description} category={item.category} date={item.createdAt} description={item.description}></ItemBox>
 
-                        <div className="row">
-                            <div className="col-md-3">
-                                <ItemBox message="this is an item box" />
-                            </div>
-                            <div className="col-md-3">
-                                <ItemBox message="this is an item box" />
-                            </div>
-                            <div className="col-md-3">
-                                <ItemBox message="this is an item box" />
-                            </div>
-                        </div>
+                        })
+                        }
+                    </div>
 
 
                     </div>
 
                 </div>
-            </div>
             </div>
         )
     }
